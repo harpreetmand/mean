a220fa943fabb7b5a796de117d7b7265555eb3bc
Harpreet Mand <harpreet@limeadvertising.com>
2017-05-30 11:46:50 -0400

    setup basic express routing

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.set('port',3000);
+app.set('port', 3000);
+
+app.get('/', function(req, res){
+    console.log("home page");
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/json', function(req, res){
+    console.log("json");
+    res
+    .status(200)
+    .json({"jsonData": true});
+})
+
+app.get('/file', function(req, res){
+    console.log("file");
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+})
 
 var server = app.listen(app.get('port'), function(){
     var port = server.address().port;

b0ff2587d6434c7fa784cbd357d257551340239b
Harpreet Mand <harpreet@limeadvertising.com>
2017-05-30 16:51:12 -0400

    added another middleware called body parser to handle post requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 var routes = require('./api/routes');
 
 app.set('port', 3000);
@@ -11,6 +12,8 @@ app.use(function(req, res, next){
     next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.use('/api', routes);
 
